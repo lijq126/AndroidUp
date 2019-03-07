c47764cd1f5d81a7970a2c011aa87fe412fd6a25
lijq <lijq@smartdot.com>
2019-03-07 18:07:31 +0800

    Tablayout应用

---
+++ app/src/main/java/com/smartdot/androidup/FragmentAdapter.java
@@ -0,0 +1,39 @@
+package com.smartdot.androidup;
+
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+
+import java.util.List;
+
+public class FragmentAdapter extends FragmentStatePagerAdapter {
+    private List<Fragment> mFragments;
+    private List<String> mTitles;
+
+    public FragmentAdapter(FragmentManager fm, List<Fragment> mFragments, List<String> mTitles) {
+        super(fm);
+        this.mFragments = mFragments;
+        this.mTitles = mTitles;
+    }
+
+    public FragmentAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int i) {
+        return mFragments.get(i);
+    }
+
+    @Override
+    public int getCount() {
+        return mFragments.size();
+    }
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return mTitles.get(position);
+    }
+}
--- app/src/main/java/com/smartdot/androidup/TabLayoutActivity.java
+++ app/src/main/java/com/smartdot/androidup/TabLayoutActivity.java
@@ -1,13 +1,49 @@
 package com.smartdot.androidup;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
 
-public class TabLayoutActivity extends AppCompatActivity {
+import com.smartdot.androidup.dummy.DummyContent;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TabLayoutActivity extends AppCompatActivity implements ItemFragment.OnListFragmentInteractionListener {
+    private ViewPager viewPager;
+    private TabLayout tabLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tab_layout);
+        viewPager = findViewById(R.id.viewpager);
+        tabLayout = findViewById(R.id.tabs);
+        initViewPager();
+
+    }
+
+    private void initViewPager() {
+
+        List<String> titles = new ArrayList<>();
+        List<Fragment> fragments = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            titles.add("标题" + i);
+            fragments.add(new ItemFragment());
+        }
+        for (String title : titles) {
+            tabLayout.addTab(tabLayout.newTab().setText(title));
+        }
+        FragmentAdapter fragmentAdapter = new FragmentAdapter(getSupportFragmentManager(), fragments, titles);
+        viewPager.setAdapter(fragmentAdapter);
+        tabLayout.setupWithViewPager(viewPager);
+        tabLayout.setTabsFromPagerAdapter(fragmentAdapter);
+    }
+
+    @Override
+    public void onListFragmentInteraction(DummyContent.DummyItem item) {
+        
     }
 }
